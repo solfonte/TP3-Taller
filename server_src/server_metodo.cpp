fe5f4a89c706098e08f8ded0462fe2b827c0592e
sol <msfontenla@fi.uba.ar>
2020-11-09 16:39:52 -0300

    cambio errores en los casos de pruebas

--- server_src/server_metodo.cpp
+++ server_src/server_metodo.cpp
@@ -10,7 +10,7 @@ MetodoInvalido::MetodoInvalido(){
 }
 
 std::string GetSinRecurso::obtener_respuesta(){
-  std::string respuesta = "HTTP 200 OK\nContent-Type: text/html\n\n";
+  std::string respuesta = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n";
   std::ifstream fs;
   fs.open(this->archivo);
   std::string contenido;
@@ -24,24 +24,24 @@ std::string GetSinRecurso::obtener_respuesta(){
 std::string GetConRecurso::obtener_respuesta(){
   std::string respuesta;
   if (this->recurso_existe){
-    respuesta = "HTTP 1.1 200 OK \n\n" + this->contenido_recurso;
+    respuesta = "HTTP/1.1 200 OK \n\n" + this->contenido_recurso;
   }else{
-    respuesta = "HTTP 404 NOT FOUND\n\n";
+    respuesta = "HTTP/1.1 404 NOT FOUND\n\n";
   }
   return respuesta;
 }
 
 std::string PostSinRecurso::obtener_respuesta(){
-  std::string respuesta = "HTTP 403 FORBIDDEN\n\n";
+  std::string respuesta = "HTTP/1.1 403 FORBIDDEN\n\n";
   return respuesta;
 }
 
 std::string PostConRecurso::obtener_respuesta(){
-  std::string respuesta = "HTTP 1.1 200 OK \n\n" + this->body;
+  std::string respuesta = "HTTP/1.1 200 OK \n\n" + this->body;
   return respuesta;
 }
 
 std::string MetodoInvalido::obtener_respuesta(){
-  std::string respuesta = "HTTP 405 METHOD NOT ALLOWED\n\n";
+  std::string respuesta = "HTTP/1.1 405 METHOD NOT ALLOWED\n\n";
   return respuesta;
 }
