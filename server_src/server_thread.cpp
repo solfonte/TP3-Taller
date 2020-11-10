dad3bdc77f6e7f662cbadb4b186355bdf69e4772
sol <msfontenla@fi.uba.ar>
2020-11-10 10:12:33 -0300

    vuelvo a meter el for

--- server_src/server_thread.cpp
+++ server_src/server_thread.cpp
@@ -55,8 +55,8 @@ void ThClient::stop(){
 
 void ThAceptador::run(){
   bool seguir_aceptando = true;
-  int cantidad_clientes;
   while (seguir_aceptando){
+    int cantidad_clientes;
     Socket peer = Socket();
     try{
       this->aceptador->aceptar(peer);
@@ -76,14 +76,13 @@ void ThAceptador::run(){
       }
     }
     cantidad_clientes = this->clientes.size();
-    //apasa r a funcioens
-  }
-  for (int i = 0; i < cantidad_clientes; i++){
-    this->clientes[i]->join();
-    this->clientes[i]->stop();
-    delete this->clientes[i];
-    this->clientes.erase(this->clientes.begin() + i);
-    cantidad_clientes --;
+    for (int i = 0; i < cantidad_clientes; i++){
+      this->clientes[i]->join();
+      this->clientes[i]->stop();
+      delete this->clientes[i];
+      this->clientes.erase(this->clientes.begin() + i);
+      cantidad_clientes --;
+    }//apasa r a funcioens
   }
 }
 bool ThClient::is_dead(){
